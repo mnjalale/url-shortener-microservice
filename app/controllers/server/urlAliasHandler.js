8a671fcab539d656c0c51ad768c8148301297546
Martin Njalale <mnjalale@gmail.com>
2016-07-01 18:16:07 +0000

    Added URL Validation

--- app/controllers/server/urlAliasHandler.js
+++ app/controllers/server/urlAliasHandler.js
@@ -2,7 +2,8 @@
 
 var path = process.cwd();
 var UrlAliases=require(path + "/app/models/urlAlias.js"),
-    UrlModule = require("url");
+    UrlModule = require("url"),
+    ValidUrl = require("valid-url");
 
 function UrlAliasHandler(){
     
@@ -25,30 +26,34 @@ function UrlAliasHandler(){
     
     this.saveUrlAlias = function(req,res){
         var url = req.params[0];
-        var origin = UrlModule.format({
-            protocol: req.protocol,
-            host: req.get('host')
-        });
         
-        
-        //Validate URL
-        var codeLength = Math.floor(Math.random() * (5-2+1)) + 2;
-        var randomCode ="";
-        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-        for(var i = 0; i < codeLength; i++) {
-            randomCode += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        
-        var urlAlias = new UrlAliases({ url: url,code:randomCode});
-        
-        urlAlias.save(function(err,result){
-            if(err){
-                throw err;
+        if(ValidUrl.isUri(url)){
+            var origin = UrlModule.format({
+                protocol: req.protocol,
+                host: req.get('host')
+            });
+            
+            
+            //Validate URL
+            var codeLength = Math.floor(Math.random() * (5-2+1)) + 2;
+            var randomCode ="";
+            var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for(var i = 0; i < codeLength; i++) {
+                randomCode += possible.charAt(Math.floor(Math.random() * possible.length));
             }
             
-            res.json({"original_url":url,"short_url": origin + "/" + randomCode});
-        });
-        
+            var urlAlias = new UrlAliases({ url: url,code:randomCode});
+            
+            urlAlias.save(function(err,result){
+                if(err){
+                    throw err;
+                }
+                
+                res.json({"original_url":url,"short_url": origin + "/" + randomCode});
+            });
+        }else{
+            res.json({"error":"URL invalid"});
+        }
         
     };
     
